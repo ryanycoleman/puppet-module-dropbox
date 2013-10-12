a69cde371907354f1b219cf8330fcd045db7fa75
Nathan R Valentine <nrvale0@gmail.com>
2013-10-12 16:56:49 -0700

    set Dropbox-recs for file watches

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,4 +10,17 @@ class dropbox(
     ensure => installed,
   }
 
+  file { 'Dropbox sysctl conf':
+    path => '/etc/sysctl.d/10-dropbox.conf',
+    ensure => file,
+    owner => 'root', group => 'root', mode => '0644',
+    content => 'fs.inotify.max_user_watches = 100000',
+  }
+
+  exec { 'Dropbox sysctl settings':
+    path => ['/bin','/sbin','/usr/bin','/usr/sbin'],
+    command => 'sysctl -p /etc/sysctl.d/10-dropbox.conf',
+    refreshonly => true,
+    subscribe => File['Dropbox sysctl conf'],
+  }
 }
