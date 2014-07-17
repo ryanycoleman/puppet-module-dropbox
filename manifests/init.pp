24c507b536970ed76d6f3b64a7752ad55839b3be
Craig Watson <craig.watson@secretsales.com>
2014-07-17 12:30:20 +0100

    Starting minor rewrite

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,47 @@
+# == Class: dropbox
+#
+# This class handles installation of DropBox. Currently, it just supports
+# Debian.
+#
+# Be warned that it will simply install a shell of the client and will NOT
+# configure it.
+#
+# === Parameters:
+#
+# [*manage_repo*]
+#   Boolean that controls whether to add the DropBox apt repository.
+#   DEFAULT: true (boolean)
+#
+# == Actions:
+#
+# * Installs the dropbox package.
+# * Optionally, adds and configures the DropBox apt repository.
+#
+# === Requires:
+#
+# * Debian $::osfamily
+#
+# === Sample Usage:
+#
+# To accept defaults:
+#
+#   include dropbox
+#
+# To disable repository management:
+#
+#   class { 'dropbox':
+#     version     => '8.6.5-621624',
+#   }
+#
+# === Authors:
+#
+# Ryan Coleman
+# Craig Watson
+#
 class dropbox(
   $manage_repo = true,
 ) {
 
-  if $manage_repo {
-    require dropbox::repo
-  }
-  
-  package { 'dropbox':
-    ensure => installed,
-  }
+  include dropbox::install
 
 }
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,10 +1,28 @@
+# == Class: dropbox::repo
+#
+# This class handles installation of the DropBox Debian apt repository.
+#
+# NOTE: This will fail on any non-Debian $::osfamily!
+#
+# == Actions:
+#
+# * Adds and configures the DropBox apt repository.
+#
+# === Requires:
+#
+# * Debian $::osfamily
+#
+# === Authors:
+#
+# Ryan Coleman
+# Craig Watson
+#
 class dropbox::repo {
   
-  case $osfamily {
+  case $::osfamily {
+
     'Debian': {
-      
       $os_lowercase = downcase($::operatingsystem)
-      
       apt::source { 'dropbox':
         location          => "http://linux.dropbox.com/${dropbox::repo::os_lowercase}",
         release           => $::lsbdistcodename,
@@ -12,13 +30,16 @@ class dropbox::repo {
         include_src       => false,
       }
       
-			apt::key { 'dropbox':
-			  key        => '5044912E',
-			  key_server => 'pgp.mit.edu',
-			}
-      
+      apt::key { 'dropbox':
+        key        => '5044912E',
+        key_server => 'pgp.mit.edu',
+      }
     }
-    
+
+    default: {
+      fail "Unsupported osfamily: ${::osfamily}"
+    }
+
   }
   
 }
