41e2120a21b4e0edd19ba88dd70910f31409fee4
Craig Watson <craig.watson@secretsales.com>
2014-07-17 14:14:51 +0100

    Fix typo

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,7 +15,7 @@
 #
 class dropbox::params {
 
-  if $dropbbox::autostart_users != 'none' {
+  if $dropbox::autostart_users != 'none' {
     $autostart_users_real = inline_template('<% if scope.lookupvar(\'dropbox::autostart_users\').is_a? Array -%><% scope.lookupvar(\'dropbox::autostart_users\').each do |autostart_user| -%><%= autostart_user %> <% end -%><% else -%><%= scope.lookupvar(\'dropbox::autostart_users\') %><% end -%>')
   } else {
     $autostart_users_real = ''
